111cf3cba0b9c16cb293efcc8493737bd1d423df
ronald vilorio <soccer23415@gmail.com>
2018-04-12 17:57:20 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def display_artist(song)
 
-    if song.artist_name == nil
+    if song.artist
        link_to "Add Artist", edit_song_path(song)
     else
        link_to song.artist_name, artist_path(song.artist)
