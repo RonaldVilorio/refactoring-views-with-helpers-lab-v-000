9289fe9f250779eec48abfeeef292192cbd70501
ronald vilorio <soccer23415@gmail.com>
2018-04-12 16:39:49 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
-    self.artist =Artist.new(name)
+    self.artist = Artist.new(name)
   end
 end
